633403f3e196809f1b04cd4bd5a353ef6e3c9471
Anisa Malik <anisaprogrammer@gmail.com>
2024-11-22 12:02:56 +0500

    javascript lecture -06 loops

---
+++ lecture-06/app.js
@@ -0,0 +1,143 @@
+
+// break and continue statement
+
+
+// for(let i = 1; i <= 10; i++){
+//     // problem statment, when i = 5 loop should stop
+//     if(i == 5){
+//         continue;   
+//     }
+//     console.log(i);
+// }
+// how to loop through from any datatype
+// loop for an object
+// const car = {
+//     name: 'mercadiez',
+//     age: 20, 
+// }
+// console.log(`Here what you will see before loop => ${Object.values(car)}`);
+
+
+// for(let i = 0; i < 2; i++){
+//     console.log(Object.values(car)[i])
+// }
+
+// how to loop through keys using for loop
+// for(let i = 0; i < 2; i++){
+//     console.log(Object.keys(car)[i])
+// }
+
+// how to convert obj into array using entires method
+
+// for(let i = 0; i < 2; i++){
+//     console.log(Object.entries(car)[i][i `]);
+//     // 
+
+// }
+
+// for in 
+
+// const car = {
+//     name: 'mercadiez',
+//     age: 20,
+//     numbers: [29, 29, 23, 28, 238]
+// }
+// console.log(car['age']);
+
+// let key = ""
+// for (let key in car) {
+//     // console.log(key);
+
+//     // console.log(car['name'])
+//     // console.log(car['age'])
+//     // console.log(car[key]);
+
+//     console.log(`Here is Key => ${key} and here is Value of Key => ${car[key]}`);
+    
+    
+// }
+
+
+
+// const users = ['jhon', 'doe', 'alex', 'ghazi']
+
+// for(let i = 0; i < users.length; i++){
+//     console.log(users[i]);
+    
+// }
+
+// for(const index in users){
+//     console.log(users[index]); 
+// }
+
+
+// foreach we loop through items from array
+
+// const prices = [29, 239, 239, 823, 238, 932]
+
+// prices.forEach((price)=>{
+//     console.log(price)
+// })
+
+// prices.forEach((value, index)=>{
+//     console.log(`Index Number is => ${index} and value is => ${value}`);
+    
+// })
+// map method on array
+
+// prices.map((price)=>{
+//     console.log(price)
+// })
+
+// for of loop 
+
+// const prices = [29, 239, 239, 823, 238, 932]
+
+// for (const value of prices){
+//     console.log(value);
+    
+// }
+
+// const user = "Jhon Doe";
+
+// for (const key of user) {
+//     console.log(key); 
+// }
+
+// JavaScript While Loop
+
+const users = ['jhon', 'alex', 'almas']
+
+// let i = 0;
+
+// while(i < users.length){
+//     console.log(users[i]);
+//     i++;
+// }
+// Do While Loop
+
+// let i = 0;
+// do{
+//     console.log(users[i]);
+//     i++;
+// }while(i < users.length)
+
+// type conversion
+
+// json
+
+// regurlar expression
+
+// js Modules
+
+
+
+// async
+
+// dom
+
+// bom
+
+// api
+
+// ajax
