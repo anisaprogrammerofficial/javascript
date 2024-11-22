efd4f0f982f5427bbea4432be6f5b3dc4970e2c4
Anisa Malik <anisaprogrammer@gmail.com>
2024-11-22 10:24:28 +0500

    javascript Loops for loop

---
+++ lecture-05/app.js
@@ -0,0 +1,119 @@
+// JavaScript Loops
+
+// const userName = "Ghazi";
+
+// const fruits = ['Banana', "orange", "peach", "amrood"]
+
+// // for(let initilze = 0; initilze < fruits.length; initilze++){
+// //     console.log(fruits[initilze])
+// // }
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// Different Kinds of Loops
+// JavaScript supports different kinds of loops:
+
+// for - loops through a block of code a number of times
+// for/in - loops through the properties of an object
+// for/of - loops through the values of an iterable object
+// while - loops through a block of code while a specified condition is true
+// do/while - also loops through a block of code while a specified condition is true
+
+// For Loop
+
+// The for statement creates a loop with 3 optional expressions:
+
+// for(expression1, expression2, expression3 ){
+//     console.log('')
+// }
+
+// for(let i=0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+// problem statement
+
+// u have a string now you have to get individual characters from that string
+
+const userName = "Orhan Ghazi";
+
+// for(let i = 0; i < userName.length; i++){
+// let star = "*";
+// console.log(userName[i]);
+// problem statement to do =>
+// o * 
+//  r ** 
+// h *** 
+// a **** 
+// n ***** 
+// G ******* 
+// }
+
+// make a table printer you have to take input from user then print appropirate table. 
+// 2 x 1 = 2;
+
+
+
+// take input from user 
+
+const printTable = document.getElementById('printTable');
+const root = document.getElementById('root')
+const error = document.createElement('p')
+let userTableInput = document.getElementById('userTableInput');
+console.log(userTableInput);
+
+const table = () => {
+    userTableInput = Number(document.getElementById('userTableInput').value);
+    const tableNumber = userTableInput;
+    const tableOuput = document.createElement('div');
+    tableOuput.setAttribute('class', 'table')
+    root.appendChild(tableOuput)
+    if (tableNumber > 0) {
+        error.innerHTML=""
+        for (let i = 1; i <= 10; i++) {
+            let res = tableNumber * i;
+            tableOuput.innerHTML += `<p class=''>${tableNumber} x ${i} = ${res}</p>`;
+        }
+
+    } else {
+        root.appendChild(error)
+        error.style.color = 'red'
+        error.innerHTML = "Please Enter Valid Table Number";
+        console.log(error);
+    }
+    
+}
+
+// event listern
+
+printTable.addEventListener('click', table)
+// 2 x 1 = 2
+
+
+
+
+// how to loop through from any datatype
+
+// type conversion
+
+// json
+
+// regurlar expression
+
+// js Modules
+
+
+
+// async
+
+// dom
+
+// bom
+
+// api
+
+// ajax
