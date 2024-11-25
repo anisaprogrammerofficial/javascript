83e0671cd2d0b7202cc58988e6415d30b1d263fb
Anisa Malik <anisaprogrammer@gmail.com>
2024-11-25 09:21:56 +0500

    javascript Html Part 01

---
+++ lecture-07/README.md
@@ -0,0 +1,16 @@
+# Lecture -07 Javascript HTML DOM Part -1  
+
+In this chapter we have studied about HTML Dom in Detail. 
+
+select html elments by id
+
+Finding HTML elements by tag name
+
+Finding HTML elements by class name
+
+Finding HTML Elements by CSS 
+Selectors
+
+applying style to element
+
+how  to add texxt into element 
---
+++ lecture-07/app.js
@@ -0,0 +1,78 @@
+// Add JS here
+// dom
+
+// select html elments by id
+// const userNaamae = 'Ghaazi Orhaan';
+// const root = document.getElementById('root')
+
+// Finding HTML elements by tag name
+
+// const root = document.getElementsByTagName('div')[0];
+
+// Finding HTML elements by class name
+
+// const root = document.getElementById('root')
+// root.setAttribute('class', 'rootElement')
+
+// const rootElement = document.getElementsByClassName('rootElement')[0]
+// console.log(rootElement.innerText)
+
+
+// Finding HTML Elements by CSS Selectors
+
+
+const root = document.querySelector('.rootElement')
+// console.log(root)
+
+// console.log(root);
+
+// console.log(root)
+
+// applying style to element
+
+root.style.width='200px';
+root.style.height='300px';
+root.style.backgroundColor='#00f';
+root.style.color='#fff';
+root.style.padding='10px'
+
+// adding attribute to html element
+
+// root.setAttribute('class', 'rootElement')
+// root.setAttribute('title', 'This is a Root Element baby')
+
+// how  to add texxt into element
+
+root.innerText = `<p> The innerHTML Property
+The easiest way to get the content of an element is by using the innerHTML property.
+The innerHTML property is useful for getting or replacing the content of HTML elements. </p>`
+console.log(root.innerText);
+
+// console.log(rootElement.innerText);
+// root.innerHTML= `<p> The innerHTML Property
+// The easiest way to get the content of an element is by using the innerHTML property.
+// The innerHTML property is useful for getting or replacing the content of HTML elements. </p>`
+
+// type conversion
+
+
+
+
+// json
+
+// regurlar expression
+
+// js Modules
+
+
+
+// async
+
+
+
+// bom
+
+// api
+
+// ajax
+
