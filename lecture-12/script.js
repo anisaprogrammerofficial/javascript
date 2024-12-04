9bc6fe60d456e19bf07a59e2e8e9b9d427235749
Anisa Malik <anisaprogrammer@gmail.com>
2024-12-04 10:32:56 +0500

    type conversions and string  methods

---
+++ lecture-12/script.js
@@ -0,0 +1,90 @@
+
+// type conversion
+
+// const productPrice = Number("20.9923");
+// const productPrice = parseFloat("20.9923");
+// const productPrice = parseInt("20.9923");
+
+// const copunDiscount = 5;
+
+// const productPriceAfterCopun = productPrice - copunDiscount;
+
+// console.log(typeof productPrice)
+// console.log(typeof copunDiscount)
+// console.log(typeof productPriceAfterCopun)
+// console.log(productPriceAfterCopun)
+
+
+// strings and String methods 
+// const userName = 'jhon Doe   ';
+// const accountStatus = true
+// console.log("Welcome" + " " + userName + "your Account stasus is: " + accountStatus);
+
+// template strings
+// console.log(`Welcome ${userName} , your account Status is: ${accountStatus}`);
+// string methods
+
+// to know string length
+
+// console.log(userName.length);
+
+// Escape Characters
+
+
+// let text = 'We are the so-called \'Vikings\' from the north.';
+// let newText = "We are the so-called \"Vikings\" from the north.";
+
+// console.log(text)
+
+// let text = 'it\'s awesome baby';
+// let backslash = "this cracter is really awesome \\ great";
+
+// let backspace = "This is awesome \b backspace"
+// console.log(backspace)
+// print text on new line
+
+// let anotherText = "I'ts prity Awesome \n I'sn't it?"
+// console.log(anotherText);
+
+// let newStyleOfString = new String("Ghazian")
+
+// console.log(newStyleOfString)
+
+// console.log(backslash)
+
+
+// Extracting String Characters
+let userName = 'Ghazian';
+
+// The charAt() method returns the character at a specified index (position) in a string:
+
+
+// console.log(userName.charAt(1));
+
+// charcode 
+
+console.log(userName.charCodeAt('0'))
+
+// numbers and number methods
+
+// array and array methods 
+
+// objects and object methods
+// Date object in detail 
+
+// json
+
+// regurlar expression
+
+// js Modules
+
+
+// async
+
+
+
+// bom
+
+// api
+
+// ajax
