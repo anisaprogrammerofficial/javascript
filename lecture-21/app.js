1cb8977e0f9cf0ef97ac77eaf8d6a191b2b020bc
anisaprogrammer <anisaprogrammer@gmail.com>
2025-01-03 12:02:37 +0500

    lecture 21 array search sort methods

---
+++ lecture-21/app.js
@@ -0,0 +1,174 @@
+console.log('javaScript Array Methods');
+
+// Array Find and Search Methods
+
+
+
+
+
+// Array indexOf()
+// let urdu_Novels = [
+//     'Carpe Diem Novel Complete by Rabia Khan ', 
+//     'Hisar E Yaar Novel By Rabia Khan', 
+//     'Dharkanon Ka Ameen Novel By Ana Ilyas', 
+//     'Satrangi Tere Rang main rang jawan Novel by Alisha Ansari'
+// ];
+
+// const fruits = ["Apple", "Orange", "Apple", "Mango","Orange"];
+
+
+// let userInput = 'Orange';
+// let searchResult = fruits.indexOf(userInput);
+// console.log(searchResult);
+
+// Array lastIndexOf()
+
+// const fruits = ["Apple", "Orange", "Apple", "Mango","Orange"];
+
+
+// let userInput = 'Orange';
+// let searchResult = fruits.lastIndexOf(userInput);
+// console.log(searchResult);
+// Array includes()
+
+// const fruits = ["Apple", "Orange", "Apple", "Mango","orange main ny kahana ha"];
+
+
+// let userInput = 'Orange';
+// let searchResult = fruits.includes(userInput);
+// console.log(searchResult);
+
+
+// // Array find()
+// const numbers = [4, 9, 16, 25, 29];
+// const findNumber = (value, index, numbers) =>{
+//     console.log(value > 18);
+//     if(value < 18){
+//         console.log(`${value} is less then 18`);
+        
+//     }else{
+//         console.log(`${value} is Greater then 18`);       
+//     }
+    
+// }
+
+// let checkNumber = numbers.find(findNumber);
+
+// let urdu_Novels = [
+//     'Carpe Diem Novel Complete by Rabia Khan ', 
+//     'Hisar', 
+//     'Dharkanon Ka Ameen Novel By Ana Ilyas', 
+//     'Satrangi Tere Rang main rang jawan Novel by Alisha Ansari',
+//     'Hisar'
+// ];
+
+// const findString = (value, index, urdu_Novels) =>{
+//     let userInput = 'Hisar';
+
+//     if(value === userInput){
+//         console.log(value);
+//     }else{
+//         // console.log(value);
+        
+//     }
+    
+// } 
+
+// urdu_Novels.find(findString);
+
+// console.log(userInput);
+
+
+// Array findIndex()
+
+// let urdu_Novels = [
+//     'Carpe Diem Novel Complete by Rabia Khan ', 
+//     'Hisar', 
+//     'Dharkanon Ka Ameen Novel By Ana Ilyas', 
+//     'Satrangi Tere Rang main rang jawan Novel by Alisha Ansari',
+//     'Hisar'
+// ];
+
+// const findString = (value, index, urdu_Novels) =>{
+//     let userInput = 'Hisar';
+
+//     if(value === userInput){
+//         console.log(value);
+//     }else{
+//         // console.log(value);
+        
+//     }
+    
+// } 
+
+// urdu_Novels.findIndex(findString);
+
+// const numbers = [4, 9, 16, 25, 29];
+// const findNumber = (value, index, numbers) =>{
+//    return (value > 18)
+// }
+
+// numbers.findIndex(findNumber);
+
+
+
+// Array findLast()
+// Array findLastIndex()
+
+
+// JavaScript Sorting Arrays
+
+// Alphabetic Sort
+
+// Array sort()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let sortedFruits = fruits.sort();
+// console.log(sortedFruits);
+
+// Array reverse()
+
+// let reveredArray = sortedFruits.reverse()
+// console.log(reveredArray);
+
+// desending order
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+
+// fruits.sort()
+// console.log(fruits);
+
+// fruits.reverse()
+// console.log(fruits);
+
+// Array toSorted()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let sortedArray = fruits.toSorted();
+// console.log(fruits);
+// console.log(sortedArray);
+
+// Array toReversed()
+
+// const months = ['jan', 'feb', 'mar', 'apr'];
+
+// let newMonths = months.toReversed();
+// // months.reverse();
+// console.log(months);
+// console.log(newMonths);
+
+// Sorting Objects
+
+
+// Numeric Sort
+// const numbers = [1,2, 4, 1, 5, 25, 100,400, 65, 6, 7, 8]
+// // numbers.sort();
+// // console.log(numbers);
+// let sortedNumbers = numbers.sort((num1, num2)=> num1 - num2)
+// console.log(sortedNumbers);
+
+// Random Sort
+// Math.min()
+
+// Math.max()
+// Home made Min()
+// Home made Max()
